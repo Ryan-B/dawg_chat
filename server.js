e5cb5de20ba945addbc7206dcd17f0d665214ca9
Ryan Bird <dawgillac11@yahoo.com>
2015-11-13 15:22:19 -0800

    Dawg chat first push

---
+++ server.js
@@ -0,0 +1,47 @@
+//Express
+var express = require('express');
+var app = express();
+
+//Static Folder
+app.use(express.static(__dirname+'/static'));
+
+var server = app.listen(1991);
+var io = require('socket.io').listen(server);
+
+var users = [];
+
+io.sockets.on('connection', function(socket) {
+	console.log("Socket connected with id: ", socket.id);
+
+	socket.on('newUser', function(data){
+		users.push({ 
+			name: data.name, 
+			socket_id: socket.id
+		})
+		// console.log(users);
+		io.emit('updateUserList', users);
+	});
+
+	socket.on('newMessage', function(data){
+		for(index in users){
+			if(users[index].socket_id === socket.id){
+				io.emit('addMessage', {
+					message: "<p class='p_chat'>"+users[index].name + ": "+ data.message+"</p>"
+				})
+				break;
+			}
+		}
+	})
+			
+
+	socket.on('disconnect', function() {
+		console.log("Socket disconnected with id: ", socket.id);
+		for(index in users){
+			if(users[index].socket_id === socket.id){
+				users.splice(index, 1);
+				io.emit('updateUserList', users);
+				break;
+			}
+		}
+	});
+});
